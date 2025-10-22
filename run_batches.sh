e2dd8c389446a843474ae4e22e249a144a993e23
Jiří Klepl <klepl@d3s.mff.cuni.cz>
2025-10-23 01:25:30 +0200

    update run_batches

--- run_batches.sh
+++ run_batches.sh
@@ -3,18 +3,21 @@
 set -euo pipefail
 
 instructions=(14 15 16)
-batch_sizes=(4 8 16 32)
+batch_sizes=(8 16)
 
 encode_boolean_flags=("--encode-boolean" "")
 force_ordered_flags=("--force-ordered" "")
 force_useful_flags=("--force-useful" "")
 
-shuffle_flags=("--no-shuffle" "--seed 42" "--seed 123")
+shuffle_flags=("--no-shuffle" "--seed 42")
 
 do_all_flags=("--do-all" "")
 
-srun --ntasks=1 --cpus-per-task=2 --mem=2G --time=1:00 --partition mpi-homo-short bash setup.sh
+num_batches=$(( ${#instructions[@]} * ${#batch_sizes[@]} * ${#encode_boolean_flags[@]} * ${#force_ordered_flags[@]} * ${#force_useful_flags[@]} * ${#shuffle_flags[@]} * ${#do_all_flags[@]} ))
 
+echo "Will create $num_batches jobs"
+
+srun --ntasks=1 --cpus-per-task=2 --mem=2G --time=1:00 --partition mpi-homo-short bash setup.sh
 
 for instr in "${instructions[@]}"; do
     for batch_size in "${batch_sizes[@]}"; do
